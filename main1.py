c6eea5f8ba52a4d5d8a9fd8896d5e6f9cd5a78ea
Ade Jerry <54339337+313Ade@users.noreply.github.com>
2023-12-01 14:23:55 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -222,12 +222,12 @@ if df is not None:
     file_name = f'{file_name}_processed.csv'
 
     # Display the download button
-    if st.download_button(
+    if st.download_button:
         label='Download processed data',
         data=df.to_csv().encode("utf-8"),
         file_name=file_name,
         mime='text/csv'
-    )
+    
 
 
 
